5a1dc482b3db72eb1655dcbae347baa3829780c1
Sreekanth Mk <sreekanth.mku@gmail.com>
2021-11-11 22:37:48 +0530

    API rate limit - 10 requests per hr per IP

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('log.request')->get('/vessels', [ApiController::class, 'index']);
+Route::middleware(['log.request', 'throttle:10,60'])->get('/vessels', [ApiController::class, 'index']);
