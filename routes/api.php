9ea755e6711d50cf7f8d369e48ab591fc54cf9d3
Sreekanth Mk <sreekanth.mku@gmail.com>
2021-11-12 03:37:25 +0530

    Rate limit 10 requests per hour

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['log.request', 'throttle:100,60'])->get('/vessels', [ApiController::class, 'index']);
-Route::middleware(['log.request', 'throttle:100,60'])->post('/vessels', [ApiController::class, 'index']);
+Route::middleware(['log.request', 'throttle:10,60'])->get('/vessels', [ApiController::class, 'index']);
+Route::middleware(['log.request', 'throttle:10,60'])->post('/vessels', [ApiController::class, 'index']);
