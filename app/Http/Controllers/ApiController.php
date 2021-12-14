a47d6dcdc96ea3e15d69b549b6ddf1275ad9b6c0
GrigorisParaskevakos <paraskevakosg@gmail.com>
2021-12-14 06:44:35 +0200

    routes and controllers

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
+/**
+ * We need to use the Trait ApiResponser in ApiController
+ * doing that we can use the trait directly in all other controllers
+ * since they are extended
+ */
 class ApiController extends Controller
 {
-    //
+    use ApiResponser; //we can use every trait method directly in our Controllers
 }
---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * PHP implements a way to reuse code called Traits. 
+ * Traits are a mechanism for code reuse in single inheritance languages such as PHP
+ * A Trait is intended to reduce some limitations of single inheritance 
+ * by enabling a developer to reuse 
+ * sets of methods freely 
+ * in several independent classes living in different class hierarchies
+ * 
+ * A Trait is similar to a class, 
+ * but only intended to group functionality in a fine-grained and consistent way
+ * 
+ * It is not possible to instantiate a Trait on its own
+ * 
+ * It is an addition to traditional inheritance and enables horizontal composition of behavior, 
+ * that is, 
+ * the application of class members without requiring inheritance
+ */
+
+namespace App\Traits;
+
+/**
+ * The Illuminate\Support\Collection class provides a fluent, 
+ * convenient wrapper for working with arrays of data
+ * 
+ * https://laravel.com/docs/8.x/collections 
+ */
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+trait ApiResponser
+{
+    /**
+     * Receives the data to be returned
+     * and the code of that response
+     */
+    private function successResponse($data, $code)
+    {
+        Log::info(response()->json($data, $code));
+        return response()->json($data, $code);
+    }
+
+    protected function errorResponse($message, $code)
+    {
+        Log::error(response()->json(['error' => $message, 'code' => $code], $code));
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+
+    protected function showAll(Collection $collection, $code = 200)
+    {
+        Log::info(response()->json(['data_collection' => $collection], $code));
+        return response()->json(['data_collection' => $collection], $code);
+    }
+
+    protected function showOne(Model $model, $code)
+    {
+        Log::info(response()->json(['data_collection' => $model], $code));
+        return response()->json(['data_model' => $model], $code);
+    }
+}
