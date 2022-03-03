d35bb4b8b4191b24be92a42bc357ca1d15afdebd
Vasilis CodeX <vasilis.tris@gmail.com>
2022-03-03 15:49:32 +0200

    middleware for logging requests

--- app/app/Http/Middleware/LogIncomingRequests.php
+++ app/app/Http/Middleware/LogIncomingRequests.php
@@ -7,7 +7,14 @@ use Illuminate\Http\Request;
 
 class LogIncomingRequests {
     public function handle(Request $request, Closure $next) {
-        
+        $path = $request->path();
+        $ip = $request->ip();
+        $payload = $request->getContent();
+
+        print_r($path);
+        print_r($ip);
+        print_r($payload);
+
         return $next($request);
     }
 }
--- app/routes/api.php
+++ app/routes/api.php
@@ -4,9 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PositionsController;
 
-Route::get('positions/{mmsi}', [PositionsController::class, 'getPositionsByVesselId'])->where('mmsi', '^[0-9]+(,[0-9]+)*');
-Route::get('positions/{lat}/{lon}', [PositionsController::class, 'getPositionsByLatLon']);
-Route::get('positions', [PositionsController::class, 'getPositionsByStamp']);
+Route::get('positions/{mmsi}', [PositionsController::class, 'getPositionsByVesselId'])
+    ->where('mmsi', '^[0-9]+(,[0-9]+)*')
+    ->middleware('log.incoming.requests');
+
+Route::get('positions/{lat}/{lon}', [PositionsController::class, 'getPositionsByLatLon'])
+    ->middleware('log.incoming.requests');
+
+Route::get('positions', [PositionsController::class, 'getPositionsByStamp'])
+    ->middleware('log.incoming.requests');
 
 // Route not found fallback
 Route::fallback(function () {
