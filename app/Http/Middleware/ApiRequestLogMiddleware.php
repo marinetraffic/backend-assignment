43d27eaab38d13e1ce41b30e63e6e9de8be8e87f
georgetere <georget@eight8.gr>
2022-06-02 20:20:45 +0300

    Create middleware for Request Logging

---
+++ app/Http/Middleware/ApiRequestLogMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\ApiRequestLogs;
+use Closure;
+use Illuminate\Http\Request;
+
+class ApiRequestLogMiddleware
+{
+    public function __construct(ApiRequestLogs $log)
+    {
+        $this->log = $log;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+
+    public function terminate($request, $response)
+    {
+        $this->log->url = $request->fullUrl();
+        $this->log->method = $request->method();
+        $this->log->body = $request->getContent();
+        $this->log->ip = $request->ip();
+        $this->log->status_code = $response->getStatusCode();
+        $this->log->created_at = now();
+        $this->log->save();
+    }
+}
---
+++ database/migrations/2022_06_02_171619_create_api_request_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_request_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->string('method');
+            $table->string('body');
+            $table->string('ip');
+            $table->string('status_code');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api_request_logs');
+    }
+};
