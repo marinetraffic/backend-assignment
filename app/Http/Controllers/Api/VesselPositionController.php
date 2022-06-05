039d25fc432675002c4fdc04d7dab1abfea322a9
saimpot <saimpot@gmail.com>
2022-06-06 01:37:05 +0300

    Changed the `VesselPositionService` type hint in the controller to use the interface instead of the implementation

--- app/Http/Controllers/Api/VesselPositionController.php
+++ app/Http/Controllers/Api/VesselPositionController.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Pagination\Paginator;
 
 class VesselPositionController extends Controller
 {
-    public function get(RetrieveVesselPositionRequest $request, VesselPositionService $vesselPositionService): Paginator
+    public function get(RetrieveVesselPositionRequest $request, VesselPositionServiceInterface $vesselPositionService): Paginator
     {
         return $vesselPositionService->getVesselPosition($request);
     }
