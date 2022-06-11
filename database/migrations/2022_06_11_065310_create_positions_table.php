02eb81a99ad2bd4bf27e787e0fb8057cb4dae0b0
infinitypaul <infinitypaul@live.com>
2022-06-11 08:29:42 +0100

    migration done

---
+++ database/migrations/2022_06_11_065310_create_positions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('mmsi');
+            $table->boolean('status')->default(false);
+            $table->integer('station_id');
+            $table->integer('speed');
+            $table->decimal('longitude', 10, 7);
+            $table->decimal('latitude', 10, 7);
+            $table->integer('course');
+            $table->integer('heading');
+            $table->string('rot')->nullable();
+            $table->bigInteger('timestamp');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+};
