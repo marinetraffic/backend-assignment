598b491ead84252e945c6f37b3bd6f931b8f071a
KC-dev <60586418+KCh-dev@users.noreply.github.com>
2022-07-04 18:56:30 +0300

    added routes for vessel requests

--- vessel-tracks/routes/api.php
+++ vessel-tracks/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VesselRequestsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/position',[VesselRequestsController::class,'getPosition']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
