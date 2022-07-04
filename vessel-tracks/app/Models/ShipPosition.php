8fcbf7124f63bd2b8f4e3319ff83f4758ce992b8
KC-dev <60586418+KCh-dev@users.noreply.github.com>
2022-07-04 18:54:10 +0300

    added ShipPosition model

--- vessel-tracks/app/Models/ShipPosition.php
+++ vessel-tracks/app/Models/ShipPosition.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\ShipPositionTraits;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ShipPosition extends Model
 {
-    use HasFactory;
+    use HasFactory, ShipPositionTraits;
 
     protected $guarded = ['id'];
 
