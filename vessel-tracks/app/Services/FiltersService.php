d823e389198fb11d446fe0ac76c31b9326925684
KC-dev <60586418+KCh-dev@users.noreply.github.com>
2022-07-05 18:00:55 +0300

    fixed positions query error, when no filter is present

--- vessel-tracks/app/Services/FiltersService.php
+++ vessel-tracks/app/Services/FiltersService.php
@@ -12,23 +12,23 @@ class FiltersService{
     public function handle(Request $request) {
 
         $params = $request->only('mmsi','time','latlong');
-        $query = ShipPosition::all();
+        $query = ShipPosition::all()->makeHidden(['created_at','updated_at' ]);
 
         foreach ($params as $key => $value) {
             $filter = 'filterBy'.$key;
             try{
-                $query = ShipPosition::$filter($value);
+                $query = ShipPosition::$filter($value)->get()->makeHidden(['created_at','updated_at' ]);
             }
             catch (\Exception $exception){
                 return $exception->getMessage();
             }
         }
 
-        $query= $query->get()->makeHidden(['created_at','updated_at' ]);
+
+
         ContentTypeService::handle($query, $request);
 
         return $query;
-
 
     }
 
