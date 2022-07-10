a2abdb5630d1cf5190739d32c5c956bc2748345f
te0mand1 <theodor.mandilaras@up2metric.com>
2022-07-10 13:59:13 +0300

    migrating and populating the database

---
+++ database/migrations/2022_07_10_094805_create_tracks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->bigInteger('mmsi');
+            $table->integer('status');
+            $table->unsignedBigInteger('stationId');
+            $table->integer('speed');
+            $table->float('lon');
+            $table->float('lat');
+            $table->integer('course');
+            $table->integer('heading');
+            $table->string("rot")->default("");
+            $table->unsignedInteger("timestamp");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+};
